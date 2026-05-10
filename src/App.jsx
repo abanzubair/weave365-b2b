aefaf68f827475b746e9b8da1335cb512776a15b
Aban Zubair <abanzubair@gmail.com>
2026-05-10 15:25:58 +0530

    removed comingsoon

--- src/App.jsx
+++ src/App.jsx
@@ -270,7 +270,7 @@ export default function App() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [routerNavigate]);
 
-  return <ComingSoon />;
+  // return <ComingSoon />;
 
   return (
     <>
