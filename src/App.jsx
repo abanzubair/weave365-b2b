910ebbdf85f5dc793f81e2a1d74f49a447ee5541
Aban Zubair <abanzubair@gmail.com>
2026-05-11 23:19:01 +0530

    activate site

--- src/App.jsx
+++ src/App.jsx
@@ -308,7 +308,7 @@ export default function App() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [routerNavigate]);
 
-  return <ComingSoon />;
+  // return <ComingSoon />;
 
   return (
     <>
